efe5cca0468184c2ee80ceb74b3dd7b0828227e1
zhufeng <zhufeng3612@163.com>
2021-03-26 15:43:16 +0800

    channel2

---
+++ e15_channel2/src/main.rs
@@ -0,0 +1,17 @@
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+
+fn main() {
+    let (tx,rx) = mpsc::channel();
+    thread::spawn(move ||{
+       let vars = vec![String::from("hi"),String::from("from"),String::from("thread")];
+        for v in vars{
+            tx.send(v).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for re in rx{
+        println!("got ：{}",re);
+    }
+}
