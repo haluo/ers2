0fa1892314cd03a20e5ded9f7e0160cc6d32ea3d
zhufeng <zhufeng3612@163.com>
2021-03-26 15:09:18 +0800

    thread2

---
+++ e14_thread2/src/main.rs
@@ -0,0 +1,11 @@
+use std::thread;
+//需要添加move将v移动到线程内部， 因为不获取所有权的话 线程内部，不知道v能在
+//主线程中存活多久，可能被提前drop掉
+
+fn main() {
+    let v = vec![1,2,3];
+    let handle = thread::spawn(move||{
+       println!("v = {:?}",v) ;
+    });
+    handle.join().unwrap();
+}
