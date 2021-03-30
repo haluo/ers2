e8a83236249524e0483116c4da771ab5e61dec0b
zhufeng <zhufeng3612@163.com>
2021-03-30 16:47:56 +0800

    macro1

---
+++ e23_macro1/mac/src/lib.rs
@@ -0,0 +1,13 @@
+
+#[macro_export]
+macro_rules! my_vec { //my_vec! 模仿vec！
+    ($($x: expr), *) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
